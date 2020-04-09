c519e87da89e7be218023f5fcd9c395786ec362f
Tomas Delvechio <tdelvechio@unlu.edu.ar>
2020-04-09 19:54:11 -0300

    se agrega manejo de la peticion y definicion de vista a cargar

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+//var_dump($_SERVER);
+$path = parse_url($_SERVER["REQUEST_URI"]);
+$http_method = $_SERVER["REQUEST_METHOD"];
 
-include 'views/index.view.php';
+if ($http_method == "GET" && $path == '/') {
+    include 'views/index.view.php';
+} else if ($http_method == "GET" && $path == '/save_turno') {
+    // Crear turno desde un modelo
+    // Validar campos
+    // Generar mensajes de ayuda o error
+    include 'views/index.view.php';
+}
